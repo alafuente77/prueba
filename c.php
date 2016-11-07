f545c58f13e394d3790f23e4a745fc512092c7cb
alafuente77 <prueba@enfasis.com>
2016-11-07 12:10:03 -0300

    se movio footer al final

--- c.php
+++ c.php
@@ -9,13 +9,13 @@ $a = print exec('id');
 <body>
 <h4> El path es :"<?echo $a; ?> </h4>
 </body>
-<footer><b><center> todos los derechos reservados!</center> </b></footer>
-
-</html>
-<?php
+<?
 //agregamos esto
 $b = print exec('id');
 //agregamos dos lineas mas!!
 $c = print exec('id');
 $d = print exec('id');
-?> 
+?>
+<footer><b><center> todos los derechos reservados!</center> </b></footer>
+</hmtl>
+
